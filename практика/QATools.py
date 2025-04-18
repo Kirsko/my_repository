b1871418cfa829c1d1eb2fc1750e2e0f85682818
madee <madeee66@gmail.com>
2025-04-18 21:42:45 +0500

    add commit

--- практика/QATools.py
+++ практика/QATools.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 
 FIRST_NAME = ['xpath', "//input[@id='firstName']"]
 LAST_NAME = ['xpath', "//input[@id='lastName']"]
